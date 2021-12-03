170bbcc172d727378004a8e23f73c0d7adeeed87
Shaffer443 <rafaelgouveiamelo@yahoo.com.br>
2021-12-03 10:56:18 -0300

    Aula 05 - Exercício 1

---
+++ aula05_exercicio01.py
@@ -0,0 +1,15 @@
+# Aula 05 | Exercício 01
+
+# Escreva uma rotina que crie uma borda ao redor de uma palavra para destaca-la
+# como sendo titulo.
+# A rotina deve receber como parâmetro a palavra a ser destacada.
+# O tamanho da caixa de texto deverá ser adpatável de acordo com o tamanho da palavra.
+
+def texto():
+    nome = input('Digite uma palavra: ')
+    letras = len(nome)
+    print('+','-' *letras,'+')
+    print('|', nome, '|')
+    print('+','-' *letras,'+')
+
+texto()
